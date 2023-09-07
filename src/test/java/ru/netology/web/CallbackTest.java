8770a6a0cb4d9d932dacdacb8a07cded6068b1d9
Ekaterina Korotkova <ekaterinakorotkova300799@gmail.com>
2023-09-07 19:18:34 +0300

    add javaTestClass

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,19 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CallbackTest {
+    @Test
+    void shouldSubmitForm() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+71234567890");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(".paragraph").shouldHave(exactText("Ваша успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
